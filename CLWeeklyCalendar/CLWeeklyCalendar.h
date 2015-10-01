e3c20b2fcf34c69fec4dd82f2472f671ab6a0466
Emilio Pavia <emilio.pavia@gmail.com>
2015-10-01 10:36:14 +0200

    Add a target to the Xcode project to build a Framework

---
+++ CLWeeklyCalendar/CLWeeklyCalendar.h
@@ -0,0 +1,20 @@
+//
+//  CLWeeklyCalendar.h
+//  CLWeeklyCalendar
+//
+//  Created by Emilio Pavia on 01/10/15.
+//  Copyright © 2015 Caesar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CLWeeklyCalendar.
+FOUNDATION_EXPORT double CLWeeklyCalendarVersionNumber;
+
+//! Project version string for CLWeeklyCalendar.
+FOUNDATION_EXPORT const unsigned char CLWeeklyCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLWeeklyCalendar/PublicHeader.h>
+
+#import <CLWeeklyCalendar/CLWeeklyCalendarView.h>
+
